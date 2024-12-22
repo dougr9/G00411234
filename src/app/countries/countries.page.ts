17551d31220f4c643422fcf9b3f8de8608ad7ac9
dougr9 <doug.a.rodrigues@hotmail.com>
2024-12-22 12:41:30 +0000

    Page Countries is now functional and display the flag and name of the country (from restcountries.com) that the user searched in the home page. It also contains the buttons to navegate to the News and Weather pages

--- src/app/countries/countries.page.ts
+++ src/app/countries/countries.page.ts
@@ -1,20 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardTitle, IonButton } from '@ionic/angular/standalone';
+import { MyDataService } from '../services/my-data.service';
+import { MyHttpService } from '../services/my-http.service';
+import { HttpOptions } from '@capacitor/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-countries',
   templateUrl: './countries.page.html',
   styleUrls: ['./countries.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonButton, IonCard, IonCardHeader, IonCardTitle, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class CountriesPage implements OnInit {
 
-  constructor() { }
+  keyword: string= "";
+  countryInfo!: any;
+
+  options: HttpOptions = {
+    url: "https://restcountries.com/v3.1/name/"
+  }
+
+  constructor(private ds:MyDataService, private mhs:MyHttpService, private router: Router) { }
 
   ngOnInit() {
+    this.getKw();
   }
+
+  async getKw() {
+    this.keyword = await this.ds.get('kw');
+    this.options.url = this.options.url.concat(this.keyword);
+    let result = await this.mhs.get(this.options);
+    this.countryInfo = result.data;
+    console.log(JSON.stringify(this.countryInfo));
+  }
+
+  async openNews() {
+      this.router.navigate(['/news'])
+  }
+
+  async openWeather() {
+    this.router.navigate(['/weather'])
+}
 
 }
