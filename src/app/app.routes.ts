2c7d49d7fa620fb0e36aeacd5d5e815dae2ed307
dougr9 <doug.a.rodrigues@hotmail.com>
2024-12-22 11:31:08 +0000

    Included services (MyData and MyHttp) and Pages (Countries, Settings, Weather and News). Updated main.ts and created the home interface

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,4 +10,20 @@ export const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full',
   },
+  {
+    path: 'countries',
+    loadComponent: () => import('./countries/countries.page').then( m => m.CountriesPage)
+  },
+  {
+    path: 'settings',
+    loadComponent: () => import('./settings/settings.page').then( m => m.SettingsPage)
+  },
+  {
+    path: 'news',
+    loadComponent: () => import('./news/news.page').then( m => m.NewsPage)
+  },
+  {
+    path: 'weather',
+    loadComponent: () => import('./weather/weather.page').then( m => m.WeatherPage)
+  },
 ];
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonButton } from '@ionic/angular/standalone';
+import { MyDataService } from '../services/my-data.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [FormsModule, IonInput, IonButton, IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
-  constructor() {}
+
+  keyword: string = "";
+  constructor(private router: Router, private ds:MyDataService) {}
+
+  async openCountries() {
+    await this.ds.set("kw", this.keyword);
+    this.router.navigate(['/countries'])
+  }
 }
